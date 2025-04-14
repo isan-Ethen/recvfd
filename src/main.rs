fe140fbde202d849e3b19b6383a54d50f3fd8066
isan <m21029@g.metro-cit.ac.jp>
2025-04-14 18:50:38 +0900

    feat: Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::fs::File;
+use std::io::{self, Read};
+use std::os::unix::io::{FromRawFd, RawFd};
+
+fn from_syscall_error(error: syscall::Error) -> io::Error {
+    io::Error::from_raw_os_error(error.errno as i32)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let fd_path = format!("chan:{}", "/tmp/redox-wayland-99");
+
+    println!("receivd file descriptor");
+    let received_fd = syscall::open(fd_path, syscall::O_RDWR).map_err(from_syscall_error)?;
+
+    println!("raw fd: {}", received_fd);
+
+    println!("as raw fd");
+    let mut file = unsafe { File::from_raw_fd(received_fd as RawFd) };
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    println!("File contents:\n{}", contents);
+
+    Ok(())
+}
