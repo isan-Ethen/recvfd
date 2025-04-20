582100f07dfd766be061130bf4b2476ee8bf7851
isan <m21029@g.metro-cit.ac.jp>
2025-04-20 12:58:09 +0900

    wip: Change socke to raw ipcd resource.

--- src/main.rs
+++ src/main.rs
@@ -12,54 +12,7 @@ fn from_syscall_error(error: syscall::Error) -> io::Error {
     io::Error::from_raw_os_error(error.errno as i32)
 }
 
-fn listen_gate(path: &str) -> Result<RawFd> {
-    println!("make socket");
-    let gate = unsafe { socket(libc::AF_UNIX, libc::SOCK_STREAM, 0) };
-    if gate < 0 {
-        return Err(io::Error::last_os_error());
-    }
-
-    let c_path = CString::new(path)
-        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "path contains null bytes"))?;
-
-    println!("initialize gate_addr");
-    let mut gate_addr: libc::sockaddr_un = unsafe { mem::zeroed() };
-    gate_addr.sun_family = libc::AF_UNIX as libc::sa_family_t;
-
-    let path_bytes = c_path.as_bytes_with_nul();
-
-    println!("check len of path");
-    if path_bytes.len() > gate_addr.sun_path.len() {
-        return Err(io::Error::new(
-            io::ErrorKind::InvalidInput,
-            "path is too long",
-        ));
-    }
-
-    println!("write path to gate_addr");
-    for (i, &byte) in path_bytes.iter().enumerate() {
-        gate_addr.sun_path[i] = byte as libc::c_char;
-    }
-
-    println!("bind socket");
-    let bind_result = unsafe {
-        bind(
-            gate,
-            &gate_addr as *const _ as *const libc::sockaddr,
-            mem::size_of::<libc::sockaddr_un>() as libc::socklen_t,
-        )
-    };
-    println!("bind result: {}", bind_result);
-    if bind_result < 0 {
-        let err = io::Error::last_os_error();
-        unsafe { libc::close(gate) };
-        return Err(err);
-    }
-
-    Ok(gate)
-}
-
-fn main() -> Result<()> {
+fn print_fds() -> Result<()> {
     let files_fd = syscall::open(
         "/scheme/thisproc/current/filetable",
         syscall::O_RDONLY as usize,
@@ -70,6 +23,11 @@ fn main() -> Result<()> {
     println!("read to string");
     file.read_to_string(&mut contents)?;
 
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    print_fds()?;
     println!("file contents:\n{}", contents);
 
     let fd_path = "/tmp/uds/test";
@@ -77,41 +35,23 @@ fn main() -> Result<()> {
     println!("scheme path: {}", scheme_path);
 
     println!("listen gate");
-    let socket_fd = listen_gate(&scheme_path)?;
-    println!("accept socket");
-    let conn_fd = unsafe {
-        libc::accept(
-            socket_fd.try_into().unwrap(),
-            std::ptr::null_mut(),
-            std::ptr::null_mut(),
-        )
-    };
-    if conn_fd < 0 {
-        return Err(io::Error::last_os_error());
-    }
-    println!("call named dup");
-    let fd = syscall::dup(conn_fd.try_into().expect("invalid argument"), b"recvfd")
+    let scheme_fd = syscall::open(scheme_path, syscall::O_CREAT | syscall::O_RDWR)
         .map_err(from_syscall_error)?;
+    println!("accept socket");
+    let gate = syscall::dup(scheme_fd, b"listen").map_err(from_syscall_error)?;
+    print_fds()?;
+    println!("call named dup");
+    let fd = syscall::dup(gate, b"/scheme/file/home/user/test").map_err(from_syscall_error)?;
     println!("raw fd: {}", fd);
 
     let message = "hello from receiver";
     let res = unsafe {
         write(
-            socket_fd.try_into().unwrap(),
+            fd,
             message.as_ptr() as *const std::os::raw::c_void,
             message.len(),
         )
     };
-
-    // println!("as raw fd");
-    // let mut file = unsafe { File::from_raw_fd(fd as RawFd) };
-    // // let mut file = unsafe { File::from_raw_fd(conn_fd as RawFd) };
-
-    // let mut contents = String::new();
-    // println!("read to string");
-    // file.read_to_string(&mut contents)?;
-
-    // println!("file contents:\n{}", contents);
 
     Ok(())
 }
